208cf8d796786140135d4145a7861d865bdab38a
dindaf13 <dindafauziaeni13@gmail.com>
2024-11-06 17:12:14 +0700

    add IsNotEmpty

--- src/modules/prescription_redemptions/dto/create-prescription_redemption.dto.ts
+++ src/modules/prescription_redemptions/dto/create-prescription_redemption.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsArray, ValidateNested } from "class-validator";
+import { IsArray, IsNotEmpty, ValidateNested } from "class-validator";
 import { CreateTransactionDto } from "src/modules/transactions/dto/create-transaction.dto";
 
 export class CreatePrescriptionRedemptionDto {
@@ -21,6 +21,7 @@ export class CreatePrescriptionRedemptionDto {
 
     // Ubah ini menjadi object, bukan array
     @ApiProperty({ type: CreateTransactionDto })
+    @IsNotEmpty() // Menandakan bahwa transaksi wajib diisi
     @ValidateNested() // Validasi objek
     @Type(() => CreateTransactionDto) // Konversi tipe ke CreateTransactionDto
     transaction: CreateTransactionDto; // Hanya satu transaksi
